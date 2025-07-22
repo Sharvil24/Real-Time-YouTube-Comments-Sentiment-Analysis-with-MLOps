af83ab84ef2edabe4ea573de21a092c72e3a8c48
Sharvil Wadekar <sharvilwadekar@gmail.com>
2025-07-22 02:00:26 -0400

    updated data folder

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'youtube',
+    version= '0.0.0',
+    author= 'Sharvil',
+    author_email= 'sharvilwadekar@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
